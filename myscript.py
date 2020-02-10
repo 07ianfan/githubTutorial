b3bc7b8e21791329a1ae71e1f53e43d80d08ad9d
07ianfan <07ianfan@gmail.com>
2020-02-10 11:26:36 -0800

    fixed function with try/except

--- myscript.py
+++ myscript.py
@@ -3,7 +3,13 @@ import numpy as np
 my_matrix = np.identity(4)
 print(my_matrix)
 
-def function(matrix_1, matrix_2):
-	return matrix_1 * matrix_2
+def my_function(matrix_1, matrix_2):
+	try:
+		return matrix_1 * matrix_2
+	except ValueError:
+		return None
+
 my_matrix = np.identity(4)
 my_2nd_matrix = np.random.random(size =(4,5))
+
+print(my_function(my_matrix, my_2nd_matrix))
