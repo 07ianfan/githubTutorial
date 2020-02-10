d753c979f40ebf4a0dd4cc4b4b821b99533d2f5b
07ianfan <07ianfan@gmail.com>
2020-02-10 11:21:31 -0800

    edited new content

--- myscript.py
+++ myscript.py
@@ -2,3 +2,8 @@ import numpy as np
 
 my_matrix = np.identity(4)
 print(my_matrix)
+
+def function(matrix_1, matrix_2):
+	return matrix_1 * matrix_2
+my_matrix = np.identity(4)
+my_2nd_matrix = np.random.random(size =(4,5))
