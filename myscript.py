782f78f8fe7e8ec04cecec154ab497f6fcd17522
07ianfan <07ianfan@gmail.com>
2020-02-10 11:34:49 -0800

    added warning

--- myscript.py
+++ myscript.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+
 
 my_matrix = np.identity(4)
 print(my_matrix)
@@ -7,9 +9,10 @@ def my_function(matrix_1, matrix_2):
 	try:
 		return matrix_1 * matrix_2
 	except ValueError:
+		print("matrixies are not aligned")
 		return None
 
-my_matrix = np.identity(4)
-my_2nd_matrix = np.random.random(size =(4,5))
+my_matrix = np.identity(5)
+my_2nd_matrix = np.random.random(size =(5,4))
 
-print(my_function(my_matrix, my_2nd_matrix))
+print(my_matrix.dot(my_second_matrix))
