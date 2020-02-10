4a491b44bcbd5c015cc99a5bec956a21dfe51d3a
07ianfan <07ianfan@gmail.com>
2020-02-10 11:32:02 -0800

    changed identity of first matrix, size of second matrix, imported pandas

--- myscript.py
+++ myscript.py
@@ -1,4 +1,17 @@
 import numpy as np
+import pandas as pd
+
 
 my_matrix = np.identity(4)
 print(my_matrix)
+
+def my_function(matrix_1, matrix_2):
+	try:
+		return matrix_1 * matrix_2
+	except ValueError:
+		return None
+
+my_matrix = np.identity(5)
+my_2nd_matrix = np.random.random(size =(5,4))
+
+print(my_matrix.dot(my_second_matrix))
